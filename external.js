e08a5bf1d06bf05fa223ca3a900d0d48847cefbc
Maria Simina <maria.simina_groza@yahoo.com>
2023-07-24 17:58:54 +0300

    Add function playRound

--- external.js
+++ external.js
@@ -5,12 +5,39 @@ function getComputerChoice () {
 
     let choice = choicesArray[index];
 
-    return console.log(choice);
-    
+    return choice;
 }
 
-getComputerChoice()
+function playRound (playerSelection, computerSelection) {
+    let selection = playerSelection.toLowerCase();
+    playerSelection = selection.charAt(0).toUpperCase() + selection.substring(1);
 
-//function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie";
+    }
 
-//}
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock.";
+        } else if (computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors.";
+        }
+    }
+
+    if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return "You Win! Paper beats Rock.";
+        } else if (computerSelection === "Scissors") {
+            return "You Lose! Scissors beats Paper.";
+        }
+    }
+
+    if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors.";
+        } else if (computerSelection === "Paper") {
+            return "You Win! Scissors beats Paper.";
+        }
+    }
+
+}
