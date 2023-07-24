d7e2b2479887ff1b7193a1c40764381ef9e61b89
Maria Simina <maria.simina_groza@yahoo.com>
2023-07-24 18:51:18 +0300

    Make game play 5 times and show results

--- external.js
+++ external.js
@@ -9,6 +9,7 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
+    
     let selection = playerSelection.toLowerCase();
     playerSelection = selection.charAt(0).toUpperCase() + selection.substring(1);
 
@@ -18,26 +19,55 @@ function playRound (playerSelection, computerSelection) {
 
     if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
-            return "You Lose! Paper beats Rock.";
+            return "Computer wins!";
         } else if (computerSelection === "Scissors") {
-            return "You Win! Rock beats Scissors.";
+            return "You win!";
         }
     }
 
     if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
-            return "You Win! Paper beats Rock.";
+            return "You win!";
         } else if (computerSelection === "Scissors") {
-            return "You Lose! Scissors beats Paper.";
+            return "Computer wins!";
         }
     }
 
     if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
-            return "You Lose! Rock beats Scissors.";
+            return "Computer wins!";
         } else if (computerSelection === "Paper") {
-            return "You Win! Scissors beats Paper.";
+            return "You win!";
         }
     }
 
 }
+
+function game (playRound) {
+    let computerCount = 0;
+    let playerCount = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let play = playRound(playerSelection, computerSelection);
+
+        if (play === "Computer wins!") {
+            computerCount++;
+            console.log ("Computer wins this game");
+        } else if (play === "You win!") {
+            playerCount++;
+            console.log("You win this game");
+        } else {
+            console.log("This game is a tie");
+        }
+    }
+
+    if (computerCount === playerCount) {
+        return "Final result: It's a tie!";
+    } else if (computerCount > playerCount) {
+        return "Final result: Computer wins!";
+    } else if (playerCount > computerCount) {
+        return "Final result: You win!";
+    }
+}
+
